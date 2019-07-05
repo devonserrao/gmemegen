6b75a17016f50401da3fb8f1d8e02463401ab452
Devon Serrao <devonserrao101@gmail.com>
2019-07-05 16:28:41 -0500

    Modifying the URL to include the gcloud host in the url_for for view_stocks()

--- app/main.py
+++ app/main.py
@@ -236,7 +236,7 @@ def api_stocks():
 # Gets all stocks
 @app.route('/stock', methods=["GET"])
 def view_stocks():
-    json_url = urllib.request.urlopen(url_for("api_stocks")) ### CONTINUE FROM HERE#######################################
+    json_url = urllib.request.urlopen(url_for(filename="https://8080-dot-7759942-dot-devshell.appspot.com" ,"api_stocks")) ### CONTINUE FROM HERE#######################################
     data = json.loads(json_url.read())
     print(data)
     stocks = Stock.query.order_by(Stock.id.desc()).all()
