26da4e5cf2bb2ae53c284136db33c8977a99f7d3
Devon Serrao <devonserrao101@gmail.com>
2019-07-08 17:11:06 -0500

    Going back to earlier version without packages

--- app/main.py
+++ app/main.py
@@ -15,7 +15,6 @@
 import os
 import time
 import json, urllib
-# import Stock, Portfolio
 
 from flask import (
     Flask, abort, request, redirect, url_for, render_template, g,
@@ -24,8 +23,8 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql.expression import func
 from PIL import Image, ImageDraw, ImageFont
 
-from packages.Stock import Stock
-from packages.Portfolio import Portfolio
+# from packages.Stock import Stock
+# from packages.Portfolio import Portfolio
 
 from configuration import (
     get_args, get_db_uri, get_templates_list,
@@ -57,52 +56,52 @@ Portfolio_Stocks = db.Table(
 )
 
 
-# class Stock(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(80), nullable=False)
-#     symbol = db.Column(db.String(10), nullable=False)
-#     price = db.Column(db.Float, nullable=False)
-#     portfolios_linked = db.relationship('Portfolio',
-#                                         secondary=Portfolio_Stocks,
-#                                         lazy='subquery',
-#                                         backref=db.backref('stocks_linked',
-#                                                            lazy=True)
-#                                         )
+class Stock(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    symbol = db.Column(db.String(10), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    portfolios_linked = db.relationship('Portfolio',
+                                        secondary=Portfolio_Stocks,
+                                        lazy='subquery',
+                                        backref=db.backref('stocks_linked',
+                                                           lazy=True)
+                                        )
 
-#     def __repr__(self):
-#         return '<Stock %r>' % self.id
+    def __repr__(self):
+        return '<Stock %r>' % self.id
 
-#     def serialize(self):
-#         return {
-#             "id": self.id,
-#             "name": self.name,
-#             "symbol": self.symbol,
-#             "price": self.price,
-#             "portfolios_linked": [p.owner for p in self.portfolios_linked]
-#         }
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "symbol": self.symbol,
+            "price": self.price,
+            "portfolios_linked": [p.owner for p in self.portfolios_linked]
+        }
     
-#     def custom_serialize(self):
-#         return {
-#             "id": self.id,
-#             "name": self.name,
-#             "symbol": self.symbol,
-#             "price": self.price
-#         }
+    def custom_serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "symbol": self.symbol,
+            "price": self.price
+        }
             
 
-# class Portfolio(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     owner = db.Column(db.String(80), nullable=False)
+class Portfolio(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    owner = db.Column(db.String(80), nullable=False)
 
-#     def __repr__(self):
-#         return '<Portfolio %r>' % self.id
+    def __repr__(self):
+        return '<Portfolio %r>' % self.id
 
-#     def serialize(self):
-#         return {
-#             "id": self.id,
-#             "owner": self.owner,
-#             "stocks_linked": [s.custom_serialize() for s in self.stocks_linked]
-#         }
+    def serialize(self):
+        return {
+            "id": self.id,
+            "owner": self.owner,
+            "stocks_linked": [s.custom_serialize() for s in self.stocks_linked]
+        }
 
 
 @app.before_first_request
